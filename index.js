243b2888e0cf136320f8a4c1a0f750f6e11d6b6a
shubhinav <73817075+shubhinav@users.noreply.github.com>
2020-12-22 17:29:36 +0530

    Files Added

---
+++ index.js
@@ -0,0 +1,150 @@
+const baseUrl = "https://api.punkapi.com/v2/beers?page=";
+
+const prevPage = document.querySelector(".prevPage")
+const nextPage = document.querySelector(".nextPage")
+const pageNumber = document.querySelector(".pageNumber")
+const beers = document.querySelector(".beers")
+const pageText = document.querySelector(".pageNumber")
+const abvFilter = document.querySelector(".abvFilter")
+const ibuFilter = document.querySelector(".ibuFilter")
+
+
+
+
+
+let abv = ""
+let ibu = ""
+let page = 1;
+
+
+prevPage.addEventListener("click", function(){
+    page--;
+    getBeers();
+})
+
+nextPage.addEventListener("click", function(){
+    page++;
+    getBeers();
+})
+
+abvFilter.addEventListener("change", function(e){
+    if(e.target.value === "all"){
+        abv = ""
+    }
+
+    else if(e.target.value === "weak"){
+        abv = "&abv_lt=5"
+        
+    }
+    else if(e.target.value === "mid"){
+        abv = "&abv_gt=5&abv_lt=8"
+        
+    }
+    else if(e.target.value === "strong"){
+        abv = "&abv_gt=8"
+        
+    }
+    page = 1;
+    getBeers();
+})
+
+ibuFilter.addEventListener("change", function(e){
+    if(e.target.value === "all"){
+        ibu = ""
+    }
+
+    else if(e.target.value === "weak"){
+        ibu = "&ibu_lt=40"
+        console.log("weak")
+    }
+    else if(e.target.value === "mid"){
+        ibu = "&ibu_gt=40&ibu_lt=70"
+        console.log("mid")
+    }
+    else if(e.target.value === "strong"){
+        ibu = "&ibu_gt=70"
+        console.log("high")
+    }
+    page = 1;
+    getBeers();
+})
+
+function descriptionHoverAnimation(){
+    const beerCards = document.querySelectorAll(".beer-card")
+    const descriptions = document.querySelectorAll(".description")
+
+    function beerHoverIn(){
+        
+        this.childNodes[1].childNodes[7].style.opacity="0.95";
+        this.childNodes[1].childNodes[7].style.transform="translateY(0%)"
+}
+
+    function beerHoverOut(){
+        this.childNodes[1].childNodes[7].style.opacity="0";
+        this.childNodes[1].childNodes[7].style.transform="translateY(100%)";
+    }
+
+   beerCards.forEach(beerCard=>{
+      beerCard.addEventListener("mouseenter", beerHoverIn)
+   })
+
+   beerCards.forEach(beerCard=>{
+       beerCard.addEventListener("mouseleave", beerHoverOut)
+   })
+}
+
+
+async function getBeers(){
+
+    const response = await fetch(baseUrl + page + abv + ibu);
+    const data = await response.json();
+    
+    let html = "";
+     data.forEach(element => { 
+        html += `<li class = "beer-card">
+                    <div class="beerInfo">
+                    <h1>${element.name}</h1>
+                    <h4><em>${element.tagline}</em></h4>
+                    <img class="beer-image" src="${element.image_url}">
+                    <p class = "description">
+                    <span> ${element.description} </span>
+                    <span>Pair with: ${element.food_pairing}</span>
+                    </p>
+                    </div>
+                    <p class = "values"><span>ABV: ${element.abv}</span> <span>IBU: ${element.ibu}</span></p>
+                 </li>`
+        
+    });
+
+    if(page===1){
+        prevPage.disabled = true;
+    }
+    else{
+        prevPage.disabled = false;
+    }
+
+    if(data.length<25){
+        nextPage.disabled = true;
+    }
+    else{
+        nextPage.disabled = false;
+    }
+
+
+    beers.innerHTML = html;
+    pageNumber.textContent = page;
+
+    descriptionHoverAnimation();
+
+    
+}
+
+
+
+getBeers();
+
+
+
+
+
+
